223a84876cf6d528f5f2db982863368edf3b1691
Stevecsob <steve.csobral@gmail.com>
2022-07-12 10:47:50 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,12 @@
-
+import NotificationButton from "./components/NotificationButton"
 
 function App() {
   return (
-    <h1>ola mundo</h1>
+    <>
+    <h1>Olá mundo</h1>
+    <NotificationButton />
+    </>
   )
-
 }
 
 export default App
---
+++ frontend/src/components/NotificationButton/index.tsx
@@ -0,0 +1,17 @@
+import icon from "../../assets/img/Vector.svg"
+
+import "./styles.css"
+
+function NotificationButton() {
+    return (
+        <>
+            <p>meu componente</p>
+            <div className="dsmeta-red-btn">
+                <img src={icon} alt="Notificar"/>
+            </div>
+        </>
+
+    )
+}
+
+export default NotificationButton
